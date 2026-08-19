baad5acd294f89a61a94521b1f98d70601299eed
Muhammad Ajnas <ajuajnas3535@gmail.com>
2026-08-19 12:52:02 +0530

    Add problems 51 to 52

---
+++ 02_problem_solving/51_two_sum_bruteforce.py
@@ -0,0 +1,7 @@
+numbers = [2, 7, 11, 15]
+target = 9
+
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] + numbers[j] == target:
+            print(numbers[i], numbers[j])
