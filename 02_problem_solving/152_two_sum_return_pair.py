26942cefc08c819767f7854df987ea883f678299
Muhammad Ajnas <ajuajnas3535@gmail.com>
2026-08-27 12:59:06 +0530

    Add problems 152 to  153

---
+++ 02_problem_solving/152_two_sum_return_pair.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 6, 8]
+target = 10
+
+left = 0
+right = len(numbers) - 1
+
+while left < right :
+    total = numbers[left] + numbers[right]
+    
+    if total == target :
+        print(left, numbers[right])
+        break
+
+    
+    elif total < target:
+        left += 1
+
+    else:
+        right -= 1
+    
