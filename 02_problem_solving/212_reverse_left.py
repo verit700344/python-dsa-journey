9aa43088596fd97ccf433ce3f150d1a4d20f310c
Muhammad Ajnas <ajuajnas3535@gmail.com>
2026-09-07 12:50:06 +0530

    Add problems  212

---
+++ 02_problem_solving/212_reverse_left.py
@@ -0,0 +1,16 @@
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+k = int(input("Enter k: "))
+k = k % len(numbers)
+left =  0
+right = len(numbers) - 1
+def reverse(left, right):
+    while left < right:
+        numbers[left], numbers[right] = numbers[right], numbers[left]
+        left += 1
+        right -= 1
+
+reverse(0, k - 1)
+reverse(k, len(numbers) - 1)
+reverse(0, len(numbers) - 1)
+
+print(f"The rotated array is: {numbers}")
