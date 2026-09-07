fa09cf53e34644bc34398474cff71437f72c9fb9
Muhammad Ajnas <ajuajnas3535@gmail.com>
2026-09-07 12:41:41 +0530

    Add problems  211 def

--- 02_problem_solving/211_reversal.py
+++ 02_problem_solving/211_reversal.py
@@ -3,13 +3,12 @@ k = int(input("Enter k: "))
 k = k % len(numbers)
 left =  0
 right = len(numbers) - 1
-left = 0
-right = len(numbers) - 1
-
-while left < right:
-    # swap numbers[left] and numbers[right]
-    numbers[left], numbers[right] = numbers[right], numbers[left]
-    # move both pointers
-    left += 1
-    right -= 1
-print("Reversed numbers:", numbers)
+def reverse(left, right):
+    while left < right:
+        numbers[left], numbers[right] = numbers[right], numbers[left]
+        left += 1
+        right -= 1
+reverse(0, len(numbers) - 1)
+reverse(0, k - 1)
+reverse(k, len(numbers) - 1)
+print(f"The rotated array is: {numbers}")
